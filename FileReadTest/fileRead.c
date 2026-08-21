807b14a93681d998422da887b9052a69e7f37e65
con <connorterstege2@gmail.com>
2026-08-20 19:25:31 -0700

    Update fileRead.c

--- FileReadTest/fileRead.c
+++ FileReadTest/fileRead.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+char characterize(const char* source) {
+    int i = 0;
+    
+    if (source == NULL || source[0] == '\0') {
+        return '\0';
+    }
+    
+    while (source[i] != '\0') {
+        i++;
+    }
+    return source[i - 1];
+}
